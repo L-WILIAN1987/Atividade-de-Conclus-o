1344e30000e7333cfb1f02e5c96bc73200832ed6
L-WILIAN1987 <69942630+L-WILIAN1987@users.noreply.github.com>
2020-11-30 14:13:59 -0300

    Add files via upload
    Atividade de conclusão de curso

---
+++ app/views/home/index.php
@@ -0,0 +1,206 @@
+<?php 
+    $this->view('home/cabecario');
+?>
+<!-- Navigation-->
+<?php
+            $this->view('home/menu');
+        ?>
+<!-- Page Content-->
+<div class="container-fluid p-0">
+    <!-- About-->
+    <section class="resume-section" id="about">
+        <div class="resume-section-content">
+            <h1 class="mb-0">
+                Nome
+                <span class="text-primary">LUCAS Wilian dos Santos</span>
+            </h1>
+            <div class="subheading mb-5">
+                0180 Centro · Guaíra, CP 14790-000 · (17) 999751430 ·
+                <a href="mailto:name@email.com">lucasprogramador@ifto.com</a>
+            </div>
+            <p class="lead mb-5"> Formado em Ciências Contábeis e estudante de Programação WEB
+
+            </p>
+            <div class="social-icons">
+                <a class="social-icon" href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a class="social-icon" href="#"><i class="fab fa-github"></i></a>
+                <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Experience-->
+    <section class="resume-section" id="experience">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Experiência</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Senior Auditor</h3>
+                    <div class="subheading mb-3">Auditoria</div>
+                    <p>Analise de Relatórios Contábeis
+
+
+                    </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Novembro 2020 - Presente</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0"></h3>
+                    <div class="subheading mb-3"></div>
+                    <p>
+
+                    </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary"></span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">r</h3>
+                    <div class="subheading mb-3"></div>
+                    <p>
+
+
+                    </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary"></span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0"></h3>
+                    <div class="subheading mb-3"></div>
+                    <p>
+
+                    </p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary"></span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Education-->
+    <section class="resume-section" id="education">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Educação</h2>
+            <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Universidade de Franca</h3>
+                    <div class="subheading mb-3">Bacharelado em Ciências Contábeis</div>
+                    <div>Auditoria</div>
+                    <p>GPA: 3.23</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Março 2009 - Dezembro 2012</span></div>
+            </div>
+            <div class="d-flex flex-column flex-md-row justify-content-between">
+                <div class="flex-grow-1">
+                    <h3 class="mb-0">Universidade de Franca</h3>
+                    <div class="subheading mb-3">MBA em Auditoria</div>
+                    <p>GPA: 3.56</p>
+                </div>
+                <div class="flex-shrink-0"><span class="text-primary">Março 2013 - Dezembro 2014</span></div>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Skills-->
+    <section class="resume-section" id="skills">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Habilidades</h2>
+            <div class="subheading mb-3">Auditoria e Contabilidade</div>
+            <ul class="list-inline dev-icons">
+                <li class="list-inline-item"><i class="></i></li>
+                <li class=" list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+                <li class="list-inline-item"><i class="t"></i></li>
+                <li class="list-inline-item"><i class=""></i></li>
+            </ul>
+            <div class="subheading mb-3">Fluxo de Trabalho</div>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Auditoria
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Contabilidade
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Finanças
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-check"></i></span>
+                    Departamento Pessoal
+                </li>
+            </ul>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Interests-->
+    <section class="resume-section" id="interests">
+        <div class="resume-section-content">
+            <?php 
+                    $this->view('home/form');
+                ?>
+        </div>
+    </section>
+    <hr class="m-0" />
+    <!-- Awards-->
+    <section class="resume-section" id="awards">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Awards & Certifications</h2>
+            <ul class="fa-ul mb-0">
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    1
+                    <sup>st</sup>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    1
+                    <sup>st</sup>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    2
+                    <sup>nd</sup>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    1
+                    <sup>st</sup>
+
+                </li>
+                <li>
+                    <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                    3
+                    <sup>rd</sup>
+
+                </li>
+            </ul>
+        </div>
+    </section>
+</div>
+
+<?php 
+    $this->view('home/rodape');
+?>
